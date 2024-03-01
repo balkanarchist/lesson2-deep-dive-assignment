e2cd2f0e5a701cac1b20036918fee97c2c1c15ca
balkanarchist <balkanarchist@yandex.com>
2024-03-01 07:04:48 -0700

    Added code to showdown-greatest and showdown-smallest

---
+++ showdown-smallest.py
@@ -0,0 +1,16 @@
+#Write a Python program that prompts the user to enter three numbers.
+#The program should then identify and
+#print out the smallest number among the three.
+
+n1 = int(input("Please enter a whole number (e.g., 1): "))
+n2 = int(input("Please enter another whole number: "))
+n3 = int(input("Please enter a third whole number: "))
+
+if n1 < n2 and n1 < n3:
+    print(f"{n1} is the smallest number.")
+
+elif n2 < n1 and n2 < n3:
+    print(f"{n2} is the smallest number.")
+
+else:
+    print(f"{n3} is the smallest number.")
